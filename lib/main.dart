a51ebfbacd0f3e992e6a536438c3726977e18fbf
edilyrosa <edilyrosa@gmail.com>
2022-12-08 11:09:57 -0400

    Adding project

---
+++ lib/main.dart
@@ -0,0 +1,383 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'dart:math';
+//import 'package:intl/intl.dart';
+//import 'package:beacons_plugin/beacons_plugin.dart';
+import 'package:flutter/material.dart';
+//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      new FlutterLocalNotificationsPlugin();
+
+  String _tag = "Beacons Plugin";
+  String _beaconResult = 'Not Scanned Yet.';
+  int _nrMessagesReceived = 0;
+  var isRunning = false;
+  List<String> _results = [];
+  bool _isInForeground = true;
+
+  final ScrollController _scrollController = ScrollController();
+
+  final StreamController<String> beaconEventsController =
+      StreamController<String>.broadcast();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    initPlatformState();
+
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+        new AndroidInitializationSettings('app_icon');
+    var initializationSettingsIOS =
+        IOSInitializationSettings(onDidReceiveLocalNotification: null);
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: null);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    _isInForeground = state == AppLifecycleState.resumed;
+  }
+
+  @override
+  void dispose() {
+    beaconEventsController.close();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    if (Platform.isAndroid) {
+      //Prominent disclosure
+      await BeaconsPlugin.setDisclosureDialogMessage(
+          title: "Background Locations",
+          message:
+              "[This app] collects location data to enable [feature], [feature], & [feature] even when the app is closed or not in use");
+
+      //Only in case, you want the dialog to be shown again. By Default, dialog will never be shown if permissions are granted.
+      //await BeaconsPlugin.clearDisclosureDialogShowFlag(false);
+    }
+
+    if (Platform.isAndroid) {
+      BeaconsPlugin.channel.setMethodCallHandler((call) async {
+        print("Method: ${call.method}");
+        if (call.method == 'scannerReady') {
+          _showNotification("Beacons monitoring started..");
+          await BeaconsPlugin.startMonitoring();
+          setState(() {
+            isRunning = true;
+          });
+        } else if (call.method == 'isPermissionDialogShown') {
+          _showNotification(
+              "Prominent disclosure message is shown to the user!");
+        }
+      });
+    } else if (Platform.isIOS) {
+      _showNotification("Beacons monitoring started..");
+      await BeaconsPlugin.startMonitoring();
+      setState(() {
+        isRunning = true;
+      });
+    }
+
+    BeaconsPlugin.listenToBeacons(beaconEventsController);
+
+    await BeaconsPlugin.addRegion(
+        "BeaconType1", "909c3cf9-fc5c-4841-b695-380958a51a5a");
+    await BeaconsPlugin.addRegion(
+        "BeaconType2", "6a84c716-0f2a-1ce9-f210-6a63bd873dd9");
+
+    BeaconsPlugin.addBeaconLayoutForAndroid(
+        "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25");
+    BeaconsPlugin.addBeaconLayoutForAndroid(
+        "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+
+    BeaconsPlugin.setForegroundScanPeriodForAndroid(
+        foregroundScanPeriod: 2200, foregroundBetweenScanPeriod: 10);
+
+    BeaconsPlugin.setBackgroundScanPeriodForAndroid(
+        backgroundScanPeriod: 2200, backgroundBetweenScanPeriod: 10);
+
+    beaconEventsController.stream.listen(
+        (data) {
+          if (data.isNotEmpty && isRunning) {
+            setState(() {
+              _beaconResult = data;
+              _results.add(_beaconResult);
+              _nrMessagesReceived++;
+            });
+
+            if (!_isInForeground) {
+              _showNotification("Beacons DataReceived: " + data);
+            }
+
+            print("Beacons DataReceived: " + data);
+          }
+        },
+        onDone: () {},
+        onError: (error) {
+          print("Error: $error");
+        });
+
+    //Send 'true' to run in background
+    await BeaconsPlugin.runInBackground(true);
+
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Monitoring Beacons'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Total Results: $_nrMessagesReceived',
+                    style: Theme.of(context).textTheme.headline4?.copyWith(
+                          fontSize: 14,
+                          color: const Color(0xFF22369C),
+                          fontWeight: FontWeight.bold,
+                        )),
+              )),
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (isRunning) {
+                      await BeaconsPlugin.stopMonitoring();
+                    } else {
+                      initPlatformState();
+                      await BeaconsPlugin.startMonitoring();
+                    }
+                    setState(() {
+                      isRunning = !isRunning;
+                    });
+                  },
+                  child: Text(isRunning ? 'Stop Scanning' : 'Start Scanning',
+                      style: TextStyle(fontSize: 20)),
+                ),
+              ),
+              Visibility(
+                visible: _results.isNotEmpty,
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _nrMessagesReceived = 0;
+                        _results.clear();
+                      });
+                    },
+                    child:
+                        Text("Clear Results", style: TextStyle(fontSize: 20)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(child: _buildResultsList())
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showNotification(String subtitle) {
+    var rng = new Random();
+    Future.delayed(Duration(seconds: 5)).then((result) async {
+      var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+          'your channel id', 'your channel name',
+          importance: Importance.high,
+          priority: Priority.high,
+          ticker: 'ticker');
+      var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+      var platformChannelSpecifics = NotificationDetails(
+          android: androidPlatformChannelSpecifics,
+          iOS: iOSPlatformChannelSpecifics);
+      await flutterLocalNotificationsPlugin.show(
+          rng.nextInt(100000), _tag, subtitle, platformChannelSpecifics,
+          payload: 'item x');
+    });
+  }
+
+  Widget _buildResultsList() {
+    return Scrollbar(
+      isAlwaysShown: true,
+      controller: _scrollController,
+      child: ListView.separated(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        physics: ScrollPhysics(),
+        controller: _scrollController,
+        itemCount: _results.length,
+        separatorBuilder: (BuildContext context, int index) => Divider(
+          height: 1,
+          color: Colors.black,
+        ),
+        itemBuilder: (context, index) {
+          DateTime now = DateTime.now();
+          String formattedDate =
+              DateFormat('yyyy-MM-dd – kk:mm:ss.SSS').format(now);
+          final item = ListTile(
+              title: Text(
+                "Time: $formattedDate\n${_results[index]}",
+                textAlign: TextAlign.justify,
+                style: Theme.of(context).textTheme.headline4?.copyWith(
+                      fontSize: 14,
+                      color: const Color(0xFF1A1B26),
+                      fontWeight: FontWeight.normal,
+                    ),
+              ),
+              onTap: () {});
+          return item;
+        },
+      ),
+    );
+  }
+}
+
+/*
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+*/
